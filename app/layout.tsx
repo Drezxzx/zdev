f5045c02c40f32d82a923ab447803c3cd42ad826
Andres <andres250197@hotmail.com>
2024-11-14 19:19:52 +0100

    addvercel analyticts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
-
-
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +18,8 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={inter.className}>
-        
         {children}   
+        <Analytics/>
       </body>
     </html>
   );
